8201b043b52f4dc696b77dc882b7c0db0e32d6e6
Vu Nguyen <vnguyen@logikor.com>
2026-03-30 15:27:25 -0400

    correct tbkyb

--- src/components/Monitor.js
+++ src/components/Monitor.js
@@ -170,7 +170,7 @@ const LOCATION_MAP = {
   'TBIL': ['TBILI'],
   'TBMS': ['TBMSMM'],
   'TBAKI': ['TOYOAA'],
-  'TBKY-B': ['TOYOBK'],
+  'TBKY-B': ['TOYOBK', 'TBABBY'],
   'TBKY-L': ['TBMKLK'],
   'TBKY-H': ['TOYOHK01'],
   'TBWK': ['TBWKHK'],
